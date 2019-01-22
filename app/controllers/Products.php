b42f6c83eba827df1dd7ef1064f6d4763b579437
Robel <robelseyoum3@gmail.com>
2019-01-22 05:11:16 +0000

    fetching pic and the product price

--- app/controllers/Products.php
+++ app/controllers/Products.php
@@ -11,6 +11,8 @@
 
         public function index(){
 
+            //Get all Products
+
             $products = $this->productModel->get_products();
 
             $data = [
@@ -19,6 +21,16 @@
             ];
 
             $this->view('products/index', $data);
+        }
+
+
+        public function details($id) {
+            // //Get Product Details
+            // $data['product'] = $this->Product_model->get_product_details($id);
+            
+            // //Load View
+            // $data['main_content'] = 'details';
+            // $this->load->view('layouts/main', $data);
         }
 
       
--- app/views/products/index.php
+++ app/views/products/index.php
@@ -3,11 +3,28 @@
 
 
     <?php foreach($data['products'] as $product): ?>
-    
-        <?php echo $product->title; ?> <br>
+           
+
+        <div class="col-md-4 game">
+            <div class="game-price"> <?php echo $product->price; ?> </div>
+            <a href="<?php echo URLROOT; ?>/products/details/<?php echo $product->id; ?>">
+                <img src="<?php echo URLROOT; ?>/images/<?php echo $product->image; ?>"/>
+            </a>
+            <div class="game-title">
+                <?php echo $product->title; ?>
+            </div>
+            <div class="game-add">
+                <button class="btn btn-primary" type="submit">Add To Cart</button>
+            </div>
+        </div>
     
 
     <?php endforeach; ?>
+
+
+
+
+
 
      
 
